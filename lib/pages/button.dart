6a3ce4d14e0c0eb5bfd01493e93c987bb50b008a
Theo Okagawa <okagawatheo@gmail.com>
2023-01-23 18:38:24 -0300

    adicionei o botao

--- lib/pages/button.dart
+++ lib/pages/button.dart
@@ -15,19 +15,5 @@ final a = ElevatedButton(
       start();
     }
   },
-);
-final b = ElevatedButton(
-  style: ButtonStyle(
-    minimumSize: MaterialStateProperty.resolveWith<Size?>((states) {
-      return const Size(150, 50);
-    }),
-    backgroundColor: MaterialStateProperty.resolveWith<Color?>((states) {
-      if (states.contains(MaterialState.hovered)) {
-        return const Color(0xffD0C3FF);
-      }
-      return Colors.blue; // normal color
-    }),
-  ),
-  child: const Text('Stop'),
-  onPressed: () => {},
-);
+),
+
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'dart:async';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
   @override
   State<MyHomePage> createState() => _MyHomePageState();
-  
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _minutes = 10;
-  
+
   void start() {
     Timer.periodic(const Duration(seconds: 1), (timer) {
       if (_minutes > 0) {
@@ -21,8 +19,6 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     });
   }
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -34,9 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
             gradient: LinearGradient(
                 begin: Alignment.bottomLeft,
                 end: Alignment.topRight,
-                colors: [Color(0xff09C6F9), Color(0xff045DE9
-)])),
-        
+                colors: [Color(0xff09C6F9), Color(0xff045DE9)])),
         child: Scaffold(
           backgroundColor: Colors.transparent,
           body: Column(
@@ -58,27 +52,54 @@ class _MyHomePageState extends State<MyHomePage> {
 
               SizedBox(
                 height: 200,
-                child: Container(
-                ),
+                child: Container(),
               ), // sizedbox do meio
 
               Expanded(
-                child: 
-                  Container(
-                    decoration: 
-                      const BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40)),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.blueGrey, 
+                child: Container(
+                  // layer branca
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.blueGrey,
                           spreadRadius: 4,
                           blurRadius: 30,
-                          offset: Offset(-7,0),
-                          blurStyle: BlurStyle.normal)],
-
-                      color: Color.fromARGB(255, 240, 243, 250))
+                          offset: Offset(-7, 0),
+                          blurStyle: BlurStyle.normal)
+                    ],
+                    color: Color.fromARGB(255, 240, 243, 250),
                   ),
-                )
-              
+                ),
+              ),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+
+                  ElevatedButton(
+                    //start button
+                    style: ButtonStyle(minimumSize:
+                        MaterialStateProperty.resolveWith<Size?>((states) {
+                      return const Size(150, 50);
+                    }), backgroundColor:
+                        MaterialStateProperty.resolveWith<Color?>((states) {
+                      if (states.contains(MaterialState.hovered)) {
+                        return const Color(0xffD0C3FF);
+                      }
+                      return Colors.blue;
+                    })),
+                    child: const Text('Start studying'),
+                    onPressed: () {
+                      if (_minutes > 0) {
+                        start();
+                      }
+                    },
+                  ),
+                ],
+              )
             ],
           ),
         ));
