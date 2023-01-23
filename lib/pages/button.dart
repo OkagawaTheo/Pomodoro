5b1c96f402f4c216284ad31712e33aa14c18e2c4
Theo Okagawa <okagawatheo@gmail.com>
2023-01-23 17:45:31 -0300

    buttons apenas como placeholder e adicionei uma layer extra na homepage

--- lib/pages/button.dart
+++ lib/pages/button.dart
@@ -1,25 +1,22 @@
-import 'package:flutter/material.dart';
-
-final startButton = ElevatedButton(
+final a = ElevatedButton(
+  //start button
   style: ButtonStyle(
-    
-    minimumSize: MaterialStateProperty.resolveWith<Size?>((states) {
+      minimumSize: MaterialStateProperty.resolveWith<Size?>((states) {
     return const Size(150, 50);
-  }), 
-  
-  backgroundColor: MaterialStateProperty.resolveWith<Color?>((states) {
+  }), backgroundColor: MaterialStateProperty.resolveWith<Color?>((states) {
     if (states.contains(MaterialState.hovered)) {
       return const Color(0xffD0C3FF);
     }
     return Colors.blue;
   })),
-  
-
   child: const Text('Start studying'),
-  onPressed: () => {},
+  onPressed: () {
+    if (_minutes > 0) {
+      start();
+    }
+  },
 );
-
-final stopButton = ElevatedButton(
+final b = ElevatedButton(
   style: ButtonStyle(
     minimumSize: MaterialStateProperty.resolveWith<Size?>((states) {
       return const Size(150, 50);
