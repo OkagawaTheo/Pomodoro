8af8977e64133cc591ad6fef98728c017e26778e
Theo Okagawa <okagawatheo@gmail.com>
2023-01-23 20:40:30 -0300

    finalmente consegui alinhasr os itens

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,72 +34,92 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Scaffold(
           backgroundColor: Colors.transparent,
           body: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Container(
                 padding: const EdgeInsets.only(top: 120),
-                child: Row(
-                  // crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: Text('$_minutes',
+                    style: const TextStyle(color: Colors.white, fontSize: 45),
+                    textAlign: TextAlign.center),
+              ),
+              const SizedBox(
+                height: 200,
+              ),
+              Expanded(
+                child: Column(
                   children: [
-                    Text('$_minutes',
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 45),
-                        textAlign: TextAlign.center),
+                    Expanded(
+                        child: Container(
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(40),
+                            topRight: Radius.circular(40)),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.blueGrey,
+                              spreadRadius: 4,
+                              blurRadius: 30,
+                              offset: Offset(-7, 0),
+                              blurStyle: BlurStyle.normal)
+                        ],
+                        color: Color.fromARGB(255, 240, 243, 250),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 50, left: 20, right: 20, bottom: 80),
+                        child: Column(children: [
+                          Expanded(
+                              child: Row(
+                            children: [
+                              Expanded(
+                                  child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Text('Study Time', style: TextStyle(fontSize: 15, color: Colors.grey[400])),
+                                      Text('Break', style: TextStyle(fontSize: 15, color: Colors.grey[400])),
+                                    ],
+                                  ),
+                                  Expanded(
+                                      child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          children: [
+                                        ElevatedButton(
+                                          //start button
+                                          style: ButtonStyle(minimumSize:
+                                              MaterialStateProperty.resolveWith<
+                                                  Size?>((states) {
+                                            return const Size(150, 50);
+                                          }), backgroundColor:
+                                              MaterialStateProperty.resolveWith<
+                                                  Color?>((states) {
+                                            if (states.contains(
+                                                MaterialState.hovered)) {
+                                              return const Color(0xffD0C3FF);
+                                            }
+                                            return Colors.blue;
+                                          })),
+                                          child: const Text('Start studying'),
+                                          onPressed: () {
+                                            if (_minutes > 0) {
+                                              start();
+                                            }
+                                          },
+                                        )
+                                      ]))
+                                ],
+                              ))
+                            ],
+                          ))
+                        ]),
+                      ),
+                    )),
                   ],
                 ),
               ),
-
-              SizedBox(
-                height: 200,
-                child: Container(),
-              ), // sizedbox do meio
-
-              Expanded(
-                child: Container(
-                  // layer branca
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.blueGrey,
-                          spreadRadius: 4,
-                          blurRadius: 30,
-                          offset: Offset(-7, 0),
-                          blurStyle: BlurStyle.normal)
-                    ],
-                    color: Color.fromARGB(255, 240, 243, 250),
-                  ),
-                ),
-              ),
-
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-
-                  ElevatedButton(
-                    //start button
-                    style: ButtonStyle(minimumSize:
-                        MaterialStateProperty.resolveWith<Size?>((states) {
-                      return const Size(150, 50);
-                    }), backgroundColor:
-                        MaterialStateProperty.resolveWith<Color?>((states) {
-                      if (states.contains(MaterialState.hovered)) {
-                        return const Color(0xffD0C3FF);
-                      }
-                      return Colors.blue;
-                    })),
-                    child: const Text('Start studying'),
-                    onPressed: () {
-                      if (_minutes > 0) {
-                        start();
-                      }
-                    },
-                  ),
-                ],
-              )
             ],
           ),
         ));
