8d28444dd3e82509cbed6855cd79b8f2c39d5b89
Theo Okagawa <okagawatheo@gmail.com>
2023-01-23 17:44:37 -0300

    arrumei o layout e adicionei uma layer extra

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,49 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:pomodoro_app/functions/start_button.dart';
+import 'dart:async';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+  
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final int _minutes = 25;
+  int _minutes = 10;
+  
+  void start() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_minutes > 0) {
+        setState(() {
+          _minutes--;
+        });
+      }
+    });
+  }
+
+  
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
         decoration: const BoxDecoration(
             gradient: LinearGradient(
                 begin: Alignment.bottomLeft,
                 end: Alignment.topRight,
-                colors: [Color(0xff1542bf), Color(0xff51a8ff)])),
+                colors: [Color(0xff09C6F9), Color(0xff045DE9
+)])),
+        
         child: Scaffold(
           backgroundColor: Colors.transparent,
           body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  Text(
-                    '$_minutes',
-                    style: const TextStyle(fontSize: 50, color: Colors.white),
-                  ),
-                ],
+              Container(
+                padding: const EdgeInsets.only(top: 120),
+                child: Row(
+                  // crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('$_minutes',
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 45),
+                        textAlign: TextAlign.center),
+                  ],
+                ),
               ),
-              const SizedBox(
-                // sizedbox do meio
-                height: 400,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [startButton, stopButton],
-              )
+
+              SizedBox(
+                height: 200,
+                child: Container(
+                ),
+              ), // sizedbox do meio
+
+              Expanded(
+                child: 
+                  Container(
+                    decoration: const BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+
+                    color: Color.fromARGB(255, 240, 243, 250))
+                  ),
+                )
+              
             ],
           ),
         ));
